2b6f15ffff6ae0dd38342de578058cd572dff37b
MD2305 <drissmellouki96@gmail.com>
2023-08-21 20:02:26 -0700

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates keygen.
+ * Return: 0 Always.
+ */
+int main(void)
+{
+int i = 0, j = 0;
+time_t k;
+
+srand((unsigned int) time(&k));
+while (j < 2772)
+{
+i = rand() % 128;
+if ((j + i) > 2772)
+break;
+j = j + i;
+printf("%c", i);
+}
+printf("%c\n", (2772 - i));
+return (0);
+}

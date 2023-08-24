6cd14add5bca92bd11b5380c32c128b9916d98ed
MD2305 <drissmellouki96@gmail.com>
2023-08-23 18:36:33 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,12 @@
 char *_strcat(char *dest, char *src)
 {
 int c = 0, c2 = 0;
-
 while (*(dest + c) != '\0')
 {
 c++;
 }
 
-while (c >= 0)
+while (c2 >= 0)
 {
 *(dest + c) = *(src + c2);
 if (*(src + c2) == '\0')

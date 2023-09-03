b9f00584da20f37dce8b1cb2ac58ffef505a7ce8
MD2305 <drissmellouki96@gmail.com>
2023-09-03 12:32:50 -0700

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,19 @@
   */
 int main(int argc, char *argv[])
 {
-int i = 0, j = 0;
+int n1 = 0, n2 = 0;
+
 if (argc == 3)
 {
-i = atoi(argv[1]);
-j = atoi(argv[2]);
-printf("%d\n", i * j);
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+printf("%d\n", n1 *n2);
 }
 else
 {
 printf("Error\n");
+return (1);
 }
+
 return (0);
 }

3d7aa6aaa786434c1ae323d3418934aef919a722
MD2305 <drissmellouki96@gmail.com>
2023-09-15 14:15:45 -0700

    up

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,7 @@
-#include "variadic_functions.h"
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
 
 /**
   * print_all - Prints anything

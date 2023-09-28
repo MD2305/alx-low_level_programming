9cfc605d0aba3901eb4786416e9cb25a9043a8d9
MD2305 <drissmellouki96@gmail.com>
2023-09-27 17:50:06 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,10 +13,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (h->n == NULL)
-			printf("%d\n", 0);
-		else
-			printf("%d\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		nnodes++;
 	}
